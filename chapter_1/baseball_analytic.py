d2ff063ecb34a3d91aedac0ed34d02ca016a735c
AtlasTheThinker <constantineatlasromanov@gmail.com>
2023-04-19 20:54:59 +0200

    Added lines of sight

--- chapter_1/baseball_analytic.py
+++ chapter_1/baseball_analytic.py
@@ -13,8 +13,14 @@ def ball_trajectory(x):
 
 xs = [x/100 for x in list(range(201))]
 ys = [ball_trajectory(x) for x in xs]
-plt.plot(xs, ys)
-plt.title('Trajectory')
+xs2 = [0.1, 2]
+ys2 = [ball_trajectory(0.1), 0]
+xs3 = [0.2, 2]
+ys3 = [ball_trajectory(0.2), 0]
+xs4 = [0.3, 2]
+ys4 = [ball_trajectory(0.3), 0]
+plt.plot(xs, ys, xs2, ys2, xs3, ys3, xs4, ys4)
+plt.title('Trajectory with Lines of sight')
 plt.xlabel('X pos')
 plt.ylabel('Y pos')
 plt.axhline(y=0)
